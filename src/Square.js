89205611d059f03a67a6e7ad04f5ec2267a58094
Ryan Dulac <analyticrye@gmail.com>
2018-09-29 18:00:56 -0400

    more exp

--- src/Square.js
+++ src/Square.js
@@ -7,7 +7,22 @@ import './Square.css';
 
 //all react components must have a render function
 export default class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentColor: this.props.initialColor,
+        };
+
+    }
+    setCurrentColor (currentColor) {
+        this.setState( {
+            currentColor,
+            foo: true,
+        });
+    }
     render() {
+        console.log(this.state);
+        let textInput;
         // const theTitle = <strong>Bold Title</strong>;
         const elements = [
             <p>a</p>,
@@ -18,14 +33,18 @@ export default class Square extends React.Component {
             width: '100px',
             height: '100px',
             //cannot mutate props, so you use state when you need to mutate
-            backgroundColor: this.props.initialColor,
+            backgroundColor: this.state.currentColor,
         };
         //return null for no render
         // return (null);
         return (
             <div>
                 <div className="square" style={style}>
-                    { this.props.showTitle && <strong>Title</strong> }
+                    <div className="square-controls">
+                        <input ref={(element) =>{ textInput = element;}}type="text" placeholder="Color..." />
+                        <button onClick={() => { this.setCurrentColor(textInput.value) }}>Ok</button>
+                    </div>
+
                 </div>
             </div>
 
